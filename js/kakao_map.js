dc0f7607df68f1051f479a8cfcb3d806fe28e085
ryuda <ryuda0101@naver.com>
2022-09-27 16:15:39 +0900

    전체 html 작업 완료

---
+++ js/kakao_map.js
@@ -0,0 +1,18 @@
+let container = document.getElementById('.cont7 .center');
+let options = {
+    center: new kakao.maps.LatLng(37.5637, 126.9758),
+    level: 3
+};
+
+let map = new kakao.maps.Map(container, options);
+
+// 일반 지도와 스카이뷰로 지도 타입을 전환할 수 있는 지도타입 컨트롤을 생성합니다
+let mapTypeControl = new kakao.maps.MapTypeControl();
+
+// 지도에 컨트롤을 추가해야 지도위에 표시됩니다
+// kakao.maps.ControlPosition은 컨트롤이 표시될 위치를 정의하는데 TOPRIGHT는 오른쪽 위를 의미합니다
+map.addControl(mapTypeControl, kakao.maps.ControlPosition.TOPRIGHT);
+
+// 지도 확대 축소를 제어할 수 있는  줌 컨트롤을 생성합니다
+let zoomControl = new kakao.maps.ZoomControl();
+map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
