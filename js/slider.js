0e7609e315b436c98e3af9cbc12ebd2b22288ecd
ryuda <ryuda0101@naver.com>
2022-09-29 14:03:51 +0900

    작업 last

--- js/slider.js
+++ js/slider.js
@@ -1,22 +1,3 @@
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-    effect: 'fade',
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-      clickable:true,
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-
-    autoplay: {
-        delay: 1000,
-        disableOnInteraction: false,
-      }  
-  });
+// 변수 생성
+// left -33.333333%해줄 슬라이드 화면
+let slide = document.querySelectorAll(".swiper-slide");
